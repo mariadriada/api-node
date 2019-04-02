7c45de613b948f68ac3716b3a9c10059ab4ed9ac
maria <mariagiraldo4@gmail.com>
2019-04-02 10:17:22 -0500

    Server configured with express

---
+++ server.js
@@ -0,0 +1,8 @@
+const CONFIG = require('./app/config/config')
+const App = require('./app/app')
+
+
+App.listen(CONFIG.PORT, (error) => {
+    if (error) return console.log(error)
+    console.log('Server running in port: ', CONFIG.PORT)
+})
