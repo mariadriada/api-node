77b1aef8d85a93a361dbb3e48ee783001475df2c
maria <mariagiraldo4@gmail.com>
2019-04-02 11:36:55 -0500

    Extended database configuration

--- app/config/database.js
+++ app/config/database.js
@@ -2,14 +2,31 @@
 const mongoose = require('mongoose')
 const CONFIG = require('./config')
 
+
 module.exports = {
     connection: null,
+    // Set database connection
     connect: function() {
+        // If exists connection return it
         if (this.connection) return this.connection
-        return mongoose.connect(CONFIG.DB)
+
+        // Create connection       
+        mongoose.connect(CONFIG.DB)        
         .then(connection => {
             this.connection = connection
-            console.log('Connection to database sucessfull!')
-        }).catch(error => console.log(error))        
+            console.log('Connection to database sucessfull!')              
+        }).catch(error => console.log(error)) 
+
+        // Signal to diconected database ok
+        mongoose.connection.on('disconnected', () => {
+            this.connection = null
+            console.log('Disconnected database!')
+        })
+        // return connection object
+        return this.connection     
+    },    
+    //Close database connection
+    close: function() {
+        mongoose.connection.close()
     }
 }
